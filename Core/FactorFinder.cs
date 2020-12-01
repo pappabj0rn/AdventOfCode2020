e9df26ee30050f69200a54992b903541679c4ac0
Greger Olofsson <greger.olofsson@gmail.com>
2020-12-01 11:25:08 +0100

    Day 1

---
+++ Core/FactorFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core
+{
+    public class FactorFinder
+    {
+        public IEnumerable<int> Candidates { get; set; }
+
+        public IEnumerable<int> FindTwoCandidatesThatSumTo(int target)
+        {
+            foreach (var c in Candidates)
+            {
+                var remainder = target - c;
+                var c2 = Candidates.SingleOrDefault(x => x == remainder);
+                if (c2 > 0)
+                    return new[] {c, c2};
+            }
+            
+            return new int[0];
+        }
+        
+        public IEnumerable<int> FindThreeCandidatesThatSumTo(int target)
+        {
+            foreach (var c in Candidates)
+            {
+                var remainder = target - c;
+                var stage2Candidates = Candidates.Where(x => x < remainder).ToArray();
+
+                foreach (var c2 in stage2Candidates)
+                {
+                    var stage2Remainder = remainder - c2;
+                    var c3 = Candidates.SingleOrDefault(x => x == stage2Remainder);
+                    if (c3 > 0)
+                        return new[] {c, c2, c3};
+                }
+            }
+            
+            return new int[0];
+        }
+    }
+}
