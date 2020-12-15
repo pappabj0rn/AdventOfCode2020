e5807e2f1b6e0e48e07b5351b2e999023dd34db3
Greger Olofsson <greger.olofsson@gmail.com>
2020-12-15 09:50:05 +0100

    Day 14

---
+++ Core/DockingProgramInitializer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Core
+{
+    public class DockingProgramInitializerV1
+    {
+        public string[] Program { get; set; }
+        
+        private long _filterMask;
+        private long _filterValue;
+
+        private readonly Dictionary<int,long> _memory = new Dictionary<int, long>();
+
+        private readonly Regex _memRegex = new Regex(@"mem\[(\d+)\] = (\d+)");
+        
+        public void Execute()
+        {
+            foreach (var line in Program)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    SetMasks(line[7..]);
+                }
+                else
+                {
+                    var match = _memRegex.Match(line);
+                    var address = int.Parse(match.Groups[1].Value);
+                    var value = int.Parse(match.Groups[2].Value);
+                    SetMemory(address, value);
+                }
+            }
+        }
+
+        private void SetMemory(in int address, in int value)
+        {
+            var filtered = (value & ~_filterMask) | _filterValue;
+            
+            if(!_memory.ContainsKey(address))
+                _memory.Add(address,0);
+            
+            _memory[address] = filtered;
+        }
+
+        private void SetMasks(string mask)
+        {
+            _filterMask = Convert.ToInt64(mask.Replace("0", "1").Replace("X", "0"), 2);
+            _filterValue = Convert.ToInt64(mask.Replace("X", "0"), 2);
+        }
+
+        public long SumNonZeroMemory()
+        {
+            return _memory.Where(m=>m.Value != 0).Sum(m=>m.Value);
+        }
+    }
+    
+    public class DockingProgramInitializerV2
+    {
+        public string[] Program { get; set; }
+        
+        private long _filterMask;
+        private long _fixedAddressBits;
+        private readonly List<long> _floatingAddressBitValues = new List<long>();
+
+        public readonly Dictionary<long,long> Memory = new Dictionary<long, long>();
+
+        private readonly Regex _memRegex = new Regex(@"mem\[(\d+)\] = (\d+)");
+        
+        public void Execute()
+        {
+            foreach (var line in Program)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    SetMasks(line[7..]);
+                }
+                else
+                {
+                    var match = _memRegex.Match(line);
+                    var address = long.Parse(match.Groups[1].Value);
+                    var value = int.Parse(match.Groups[2].Value);
+                    SetMemory(address, value);
+                }
+            }
+        }
+
+        private void SetMemory(in long address, in int value)
+        {
+            var baseAddress = (address & ~_filterMask) | _fixedAddressBits;
+
+            var variations = (int) Math.Pow(2, _floatingAddressBitValues.Count);
+            var addressModifiers = new long[variations];
+            for (var i = 1; i < variations; i++)
+            {
+                addressModifiers[i] = GetFloatingValueIndices(i).Sum(f => _floatingAddressBitValues[f]);
+            }
+
+            foreach (var addressModifier in addressModifiers)
+            {
+                var addr = baseAddress + addressModifier;
+                
+                if(!Memory.ContainsKey(addr))
+                    Memory.Add(addr,0);
+
+                Memory[addr] = value;
+            }
+        }
+
+        private IEnumerable<int> GetFloatingValueIndices(in int val)
+        {
+            var components = new List<int>();
+            var f = 1;
+            var i = 0;
+            while (f <= val)
+            {
+                if((val & f) > 0)
+                    components.Add(i);
+                f <<= 1;
+                i++;
+            }
+            
+            return components;
+        }
+
+        private void SetMasks(string mask)
+        {
+            _filterMask = Convert.ToInt64(mask.Replace("X", "1"), 2);
+            _fixedAddressBits = Convert.ToInt64(mask.Replace("X", "0"), 2);
+
+            var n = 0;
+            _floatingAddressBitValues.Clear();
+            foreach (var c in mask.Reverse())
+            {
+                if (c == 'X')
+                {
+                    _floatingAddressBitValues.Add((long) Math.Pow(2, n));
+                }
+
+                n++;
+            }
+        }
+
+        public long SumNonZeroMemory()
+        {
+            return Memory.Where(m=>m.Value != 0).Sum(m=>m.Value);
+        }
+    }
+}
---
+++ CoreTests/DockingProgramInitializerTests.cs
@@ -0,0 +1,163 @@
+﻿using Core;
+using Xunit;
+
+namespace CoreTests
+{
+    public class DockingProgramInitializerV1Tests
+    {
+        [Fact]
+        public void Example_1()
+        {
+            var program = new[]
+            {
+                "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+                "mem[8] = 11",
+                "mem[7] = 101",
+                "mem[8] = 0"
+            };
+
+            var init = new DockingProgramInitializerV1
+            {
+                Program = program
+            };
+                
+            init.Execute();
+                
+            Assert.Equal(165, init.SumNonZeroMemory());
+        }
+
+        [Fact]
+        public void Puzzle_14A()
+        {
+            var init = new DockingProgramInitializerV1
+            {
+                Program = PuzzleInputs.Puzzle14
+            };
+                
+            init.Execute();
+                
+            Assert.Equal(6559449933360, init.SumNonZeroMemory());
+        }
+    }
+    
+    public class DockingProgramInitializerV2Tests
+    {
+        [Fact]
+        public void Example_2()
+        {
+            var program = new[]
+            {
+                "mask = 000000000000000000000000000000X1001X",
+                "mem[42] = 100",
+                "mask = 00000000000000000000000000000000X0XX",
+                "mem[26] = 1"
+            };
+
+            var init = new DockingProgramInitializerV2
+            {
+                Program = program
+            };
+                
+            init.Execute();
+                
+            Assert.Equal(208, init.SumNonZeroMemory());
+        }
+        
+        [Fact]
+        public void Example_2b()
+        {
+            var program = new[]
+            {
+                "mask = 000000000000000000000000000000X1001X",
+                "mem[42] = 100"
+            };
+
+            var init = new DockingProgramInitializerV2
+            {
+                Program = program
+            };
+                
+            init.Execute();
+
+            var memAddress = init.Memory.Keys;
+            Assert.Contains(26, memAddress);
+            Assert.Contains(27, memAddress);
+            Assert.Contains(58, memAddress);
+            Assert.Contains(59, memAddress);
+            foreach (var addr in memAddress)
+            {
+                Assert.Equal(100, init.Memory[addr]);
+            }
+        }
+        
+        [Fact]
+        public void Example_2c()
+        {
+            var program = new[]
+            {
+                "mask = 00000000000000000000000000000000X0XX",
+                "mem[26] = 1"
+            };
+
+            var init = new DockingProgramInitializerV2
+            {
+                Program = program
+            };
+                
+            init.Execute();
+
+            var memAddress = init.Memory.Keys;
+            Assert.Contains(16, memAddress);
+            Assert.Contains(17, memAddress);
+            Assert.Contains(18, memAddress);
+            Assert.Contains(19, memAddress);
+            Assert.Contains(24, memAddress);
+            Assert.Contains(25, memAddress);
+            Assert.Contains(26, memAddress);
+            Assert.Contains(27, memAddress);
+            foreach (var addr in memAddress)
+            {
+                Assert.Equal(1, init.Memory[addr]);
+            }
+        }
+        
+        [Fact]
+        public void Example_2d()
+        {
+            var program = new[]
+            {
+                "mask = X00000000000000000000000000000000000",
+                "mem[1] = 1"
+            };
+
+            var init = new DockingProgramInitializerV2
+            {
+                Program = program
+            };
+                
+            init.Execute();
+
+            var memAddress = init.Memory.Keys;
+            Assert.Contains(1, memAddress);
+            Assert.Contains(34_359_738_369, memAddress);
+            foreach (var addr in memAddress)
+            {
+                Assert.Equal(1, init.Memory[addr]);
+            }
+        }
+
+        [Fact]
+        public void Puzzle_14B()
+        {
+            var init = new DockingProgramInitializerV2
+            {
+                Program = PuzzleInputs.Puzzle14
+            };
+                
+            init.Execute();
+                
+            Assert.True(init.SumNonZeroMemory() > 2090634305407, "too low");
+            Assert.Equal(3369767240513, init.SumNonZeroMemory());
+        }
+    }
+}
