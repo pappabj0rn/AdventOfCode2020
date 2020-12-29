1d1cf2a6ee770e9c3782c2f6f3edda12eab2181e
Greger Olofsson <greger.olofsson@gmail.com>
2020-12-29 23:06:18 +0100

    Day 22.1

---
+++ Core/CrabCombat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core
+{
+    public class CrabCombat
+    {
+        public Queue<int> P1 { get; set; }
+        public Queue<int> P2 { get; set; }
+        public int RoundsPlayed { get; set; }
+        public int P1Score { get; set; }
+        public int P2Score { get; set; }
+
+        public void Battle()
+        {
+            while (P1.Any() && P2.Any())
+            {
+                RoundsPlayed++;
+                
+                var c1 = P1.Dequeue();
+                var c2 = P2.Dequeue();
+                
+                if (c1 > c2)
+                {
+                    P1.Enqueue(c1);
+                    P1.Enqueue(c2);
+                }
+                else
+                {
+                    P2.Enqueue(c2);
+                    P2.Enqueue(c1);
+                }
+            }
+
+            P1Score = CalculateScore(P1);
+            P2Score = CalculateScore(P2);
+        }
+
+        private int CalculateScore(Queue<int> deck)
+        {
+            var score = 0;
+            
+            for (var i = deck.Count; i > 0; i--)
+            {
+                score += deck.Dequeue() * i;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ CoreTests/CrabCombatTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Core;
+using Xunit;
+
+namespace CoreTests
+{
+    public class CrabCombatTests
+    {
+        [Fact]
+        public void Example_1()
+        {
+            var cc = new CrabCombat
+            {
+                P1 = new Queue<int>( new [] { 9, 2, 6, 3, 1 }),
+                P2 = new Queue<int>( new [] { 5, 8, 4, 7, 10 })
+            };
+
+            cc.Battle();
+            
+            Assert.Equal(29, cc.RoundsPlayed);
+            Assert.Equal(0, cc.P1Score);
+            Assert.Equal(306, cc.P2Score);
+        }
+        
+        [Fact]
+        public void Puzzle_22A()
+        {
+            var cc = new CrabCombat
+            {
+                P1 = new Queue<int>(PuzzleP1),
+                P2 = new Queue<int>(PuzzleP2)
+            };
+
+            cc.Battle();
+            
+            Assert.Equal(687, cc.RoundsPlayed);
+            Assert.Equal(33098, cc.P1Score);
+            Assert.Equal(0, cc.P2Score);
+        }
+        
+        private static readonly int[] PuzzleP1 = {
+            19,
+            5,
+            35,
+            6,
+            12,
+            22,
+            45,
+            39,
+            14,
+            42,
+            47,
+            38,
+            2,
+            26,
+            13,
+            30,
+            4,
+            34,
+            43,
+            40,
+            16,
+            8,
+            23,
+            50,
+            36
+        };
+        
+        private static readonly int[] PuzzleP2 = {
+            1,
+            21,
+            29,
+            41,
+            32,
+            28,
+            9,
+            37,
+            49,
+            20,
+            17,
+            27,
+            24,
+            3,
+            33,
+            44,
+            48,
+            31,
+            15,
+            25,
+            18,
+            46,
+            7,
+            10,
+            11
+        };
+    }
+}
